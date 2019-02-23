cb952fe990e4c17a32a1fa2d23f17747ebb924a6
Oleg Kolomiytsev <kolomiytsev.oleg@gmail.com>
2019-02-23 20:45:37 +0200

    Notification center functionality was tested.

---
+++ modules/notification-center/src/Main.cpp
@@ -0,0 +1,60 @@
+#include "pch.h"
+
+class BaseNotification : public Poco::Notification {
+
+};
+
+class SubNofication : public BaseNotification {
+
+};
+
+class Target
+{
+public:
+	Target(Poco::Logger& logger)
+		: logger_(logger)
+	{ }
+
+public:
+	void handleBase(BaseNotification* baseNt) {
+		logger_.information("base handler: %s", baseNt->name());
+		baseNt->release();
+	}
+	void handleSub(const Poco::AutoPtr<SubNofication>& subNt) {
+		logger_.information("sub handler: %s", subNt->name());
+	}
+
+private:
+	Poco::Logger& logger_;
+};
+
+class App : public Poco::Util::ServerApplication
+{
+protected:
+	int main(const ArgVec& args)
+	{
+		// Initializes console logger.
+		Poco::AutoPtr<Poco::FormattingChannel> channel(new Poco::FormattingChannel);
+		channel->setChannel(new Poco::ConsoleChannel());
+		channel->setFormatter(new Poco::PatternFormatter("%d/%m/%Y %H:%M:%S,%i [%p] %t"));
+		logger().setChannel(channel);
+
+		Poco::NotificationCenter nc;
+		Target target(logger());
+
+		nc.addObserver(Poco::Observer<Target, BaseNotification>(target, &Target::handleBase));
+		nc.addObserver(Poco::NObserver<Target, SubNofication>(target, &Target::handleSub));
+
+		nc.postNotification(new BaseNotification);
+		nc.postNotification(new SubNofication);
+
+		nc.removeObserver(Poco::Observer<Target, BaseNotification>(target, &Target::handleBase));
+		nc.removeObserver(Poco::NObserver<Target, SubNofication>(target, &Target::handleSub));
+
+		waitForTerminationRequest();
+
+		return Application::EXIT_OK;
+	}
+};
+
+POCO_SERVER_MAIN(App);
---
+++ modules/notification-center/src/pch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "targetver.h"
+
+//////////////////////////////////////////////////////////////////////////
+// STL includes and definitions.
+//
+
+#include <iostream>
+#include <string>
+
+//////////////////////////////////////////////////////////////////////////
+// Poco includes and definitions
+//
+
+#include "Poco/Util/ServerApplication.h"
+#include "Poco/ConsoleChannel.h"
+#include "Poco/FormattingChannel.h"
+#include "Poco/PatternFormatter.h"
+#include "Poco/NotificationCenter.h"
+#include "Poco/Observer.h"
+#include "Poco/NObserver.h"
